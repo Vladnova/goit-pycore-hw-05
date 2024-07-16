53497293a6fe8917e04edc24e4d4910a044fb09c
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-07-16 23:19:36 +0300

    finished task_1

---
+++ task_1.py
@@ -0,0 +1,23 @@
+from typing import Dict, Callable
+
+def caching_fibonacci()-> Callable[[int], int]:
+    cache: Dict[int, int] = {}
+
+    def fibonacci(n: int)-> int:
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = fibonacci(n -1) + fibonacci(n-2)
+        return cache[n]
+    
+    return fibonacci
+
+
+fib = caching_fibonacci()
+
+print(fib(10))
+print(fib(15))
